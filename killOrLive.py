fd3c5d80ca2079c505f5c0b8df775233bf5d19d7
Andres Tamez <andrestamez191@gmail.com>
2023-03-15 16:07:07 -0600

    Agregamos la vairable de poder a la clase padre y la variable de tipo de enemigo a la clase de enemigo

--- killOrLive.py
+++ killOrLive.py
@@ -1,25 +1,23 @@
 class Personaje:
     
-    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, tipo_de_poder):
         self.nombre = nombre
         self.fuerza = fuerza
         self.inteligencia = inteligencia
         self.defensa = defensa
         self.vida = vida
-        
+        self.tipo_de_poder = tipo_de_poder
         
 class Enemigo(Personaje):
     
-    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, poder_enemigo, tipo_enemigo):
-        self.poder_enemigo = poder_enemigo
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, tipo_de_poder, tipo_enemigo):
         self.tipo_enemigo = tipo_enemigo
-        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+        super().__init__(nombre, fuerza, inteligencia, defensa, vida, tipo_de_poder)
         
 class Protagonista(Personaje):
     
-    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, poder_protagonista):
-        self.poder_protagonista = poder_protagonista
-        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, tipo_de_poder):
+        super().__init__(nombre, fuerza, inteligencia, defensa, vida, tipo_de_poder)
         
 poder_posible = ['Fuego', 'Aire', 'Agua', 'Rayo', 'Tierra', 'Oscuridad']
 enemigos_posibles = ['Enano', 'Gigante', 'Mago']
