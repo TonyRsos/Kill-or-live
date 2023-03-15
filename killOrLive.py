359c75e7cdb03382350e82d09b86a90e9805a73b
TonyRsos <jtpelagio2003@gmail.com>
2023-03-15 11:09:44 -0600

    estructuramos las variables

--- killOrLive.py
+++ killOrLive.py
@@ -1,4 +1,8 @@
 class personaje:
     
     def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
-        pass
+        self.nombre = nombre
+        self.fuerza = fuerza
+        self.inteligencia = inteligencia
+        self.defensa = defensa
+        self.vida = vida
