2faab3c42b65f8dd79433f184c2aaffe5cea4449
Andres Tamez <andrestamez191@gmail.com>
2023-03-15 11:30:54 -0600

    Agregamos variables de poder, y tipo dde enemigos, y declaramos las posibles opciones de cada uno

--- killOrLive.py
+++ killOrLive.py
@@ -10,10 +10,16 @@ class Personaje:
         
 class Enemigo(Personaje):
     
-    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, poder_enemigo, tipo_enemigo):
+        self.poder_enemigo = poder_enemigo
+        self.tipo_enemigo = tipo_enemigo
         super().__init__(nombre, fuerza, inteligencia, defensa, vida)
         
 class Protagonista(Personaje):
     
-    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, poder_protagonista):
+        self.poder_protagonista = poder_protagonista
         super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+        
+poder_posible = ['Fuego', 'Aire', 'Agua', 'Rayo', 'Tierra', 'Oscuridad']
+enemigos_posibles = ['Enano', 'Gigante', 'Mago']
