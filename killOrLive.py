4cd82b4143ba5c600493eadd50d242cf3d15fc9a
Andres Tamez <andrestamez191@gmail.com>
2023-03-22 11:37:46 -0600

    mejoramos el codigo e hicimos tests

--- killOrLive.py
+++ killOrLive.py
@@ -1,30 +1,46 @@
-class Personaje:
-    
+from dataclasses import dataclass
+
+@dataclass
+class Caracteristicas:
     poder_posible = ['Fuego', 'Aire', 'Agua', 'Rayo', 'Tierra', 'Oscuridad']
+    enemigos_posibles = ['Enano', 'Gigante', 'Mago']
     
-    def __init__(self, nombre, tipo_de_poder, fuerza=0, inteligencia=0, defensa=0, vida=0):
-        self.nombre = nombre
-        self.fuerza = fuerza
-        self.inteligencia = inteligencia
-        self.defensa = defensa
-        self.vida = vida
-        self.tipo_de_poder = self.validar_tipo_de_poder(tipo_de_poder) 
-        
+    nombre: str
+    tipo_de_poder: str
+    fuerza: int = 0
+    inteligencia: int = 0
+    defensa: int = 0
+    vida: int = 0
+    
     def validar_tipo_de_poder(self, tipo_de_poder):
         if tipo_de_poder in self.poder_posible:
             return tipo_de_poder
         else:
             return self.poder_posible[0]
+
+
+class Personaje:
+    def __init__(self, caracteristicas):
+        self.nombre = caracteristicas.nombre
+        self.fuerza = caracteristicas.fuerza
+        self.inteligencia = caracteristicas.inteligencia
+        self.defensa = caracteristicas.defensa
+        self.vida = caracteristicas.vida
+        self.tipo_de_poder = caracteristicas.validar_tipo_de_poder(caracteristicas.tipo_de_poder) 
+        
         
 class Enemigo(Personaje):
-    
-    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, tipo_de_poder, tipo_enemigo):
+    def __init__(self, tipo_de_poder, tipo_enemigo, caracteristicas):
         self.tipo_enemigo = tipo_enemigo
-        super().__init__(nombre, fuerza, inteligencia, defensa, vida, tipo_de_poder)
+        super().__init__(caracteristicas, tipo_de_poder)
         
+    def validar_tipo_enemigo(self, tipo_enemigo):
+        if tipo_enemigo in self.enemigos_posibles:
+            return tipo_enemigo
+        else:
+            return self.enemigos_posibles[1]
+         
 class Protagonista(Personaje):
-    
-    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, tipo_de_poder):
-        super().__init__(nombre, fuerza, inteligencia, defensa, vida, tipo_de_poder)
+    def __init__(self, caracteristicas):
+        super().__init__(caracteristicas)
         
-enemigos_posibles = ['Enano', 'Gigante', 'Mago']
--- tests/test_personaje.py
+++ tests/test_personaje.py
@@ -1,7 +1,14 @@
-PODER_NO_PERMITIDO = 'Luz'
+from killOrLive import Personaje, Enemigo, Caracteristicas
 
-from killOrLive import Personaje
+PODER_NO_PERMITIDO = 'Luz'
+ENEMIGO_NO_POSIBLE = 'bruja'
+
+caracteristicas = Caracteristicas('andres',PODER_NO_PERMITIDO)
 
 def test_poder_posible_personaje():
-    personaje = Personaje('', PODER_NO_PERMITIDO)
+    personaje = Personaje(caracteristicas)
     assert personaje.tipo_de_poder != PODER_NO_PERMITIDO
+    
+#def test_enemigo_posible_personaje():
+  #  enemigo = Enemigo('','', ENEMIGO_NO_POSIBLE)
+  #l  assert enemigo.tipo_enemigo != ENEMIGO_NO_POSIBLE
